197cf0eabea7e5e33f3bc907d842b0917b9335b4
naohisa0820 <nao8.20y.peace@gmail.com>
2022-02-23 23:15:53 +0900

    追加修正

--- react-practice/src/__tests__/ui/page/testpage/testpage.test.tsx
+++ react-practice/src/__tests__/ui/page/testpage/testpage.test.tsx
@@ -1,37 +1,37 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import { mount, configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { BrowserRouter as Router } from "react-router-dom";
-import { TopPage } from "ui/page/top/TopPage";
+// import React from "react";
+// import { render, fireEvent } from "@testing-library/react";
+// import { mount, configure } from "enzyme";
+// import Adapter from "enzyme-adapter-react-16";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { TopPage } from "ui/page/top/TopPage";
 
-configure({ adapter: new Adapter() });
-const mockedConsole = (console.log = jest.fn());
+// configure({ adapter: new Adapter() });
+// const mockedConsole = (console.log = jest.fn());
 
-describe("unit test", () => {
-  // testing libraryで書いた場合
-  it("testing libraryでbutton click", async () => {
-    const wrapper = render(
-      <Router>
-        <TopPage />
-      </Router>
-    );
-    const WrapperButton = wrapper.getByText("Contained");
-    fireEvent.click(WrapperButton);
-    expect(mockedConsole).toBeCalledTimes(1);
-    wrapper.unmount();
-  });
+// describe("unit test", () => {
+//   // testing libraryで書いた場合
+//   it("testing libraryでbutton click", async () => {
+//     const wrapper = render(
+//       <Router>
+//         <TopPage />
+//       </Router>
+//     );
+//     const WrapperButton = wrapper.getByText("Contained");
+//     fireEvent.click(WrapperButton);
+//     expect(mockedConsole).toBeCalledTimes(1);
+//     wrapper.unmount();
+//   });
 
-  // enzymeで書いた場合
-  it("enzymeでbutton click", () => {
-    const wrapper = mount(
-      <Router>
-        <TopPage />
-      </Router>
-    );
-    const WrapperButton = wrapper.find("button").at(0);
-    WrapperButton.simulate("click");
-    expect(mockedConsole).toBeCalledTimes(1);
-    wrapper.unmount();
-  });
-});
+//   // enzymeで書いた場合
+//   it("enzymeでbutton click", () => {
+//     const wrapper = mount(
+//       <Router>
+//         <TopPage />
+//       </Router>
+//     );
+//     const WrapperButton = wrapper.find("button").at(0);
+//     WrapperButton.simulate("click");
+//     expect(mockedConsole).toBeCalledTimes(1);
+//     wrapper.unmount();
+//   });
+// });
