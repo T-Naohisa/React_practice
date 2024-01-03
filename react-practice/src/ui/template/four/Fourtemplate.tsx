817d6ea321f162b0adbd75f34c3def69b0e1e23e
naohisa0820 <nao8.20y.peace@gmail.com>
2024-01-03 11:36:49 +0900

    nodeバージョンアップ対応

--- react-practice/src/ui/template/four/Fourtemplate.tsx
+++ react-practice/src/ui/template/four/Fourtemplate.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Header } from "ui/atom/Header";
-import { Footer } from "ui/atom/Footer";
-import { FormikComponent, SampleForm } from "ui/organism/four/FormikComponent";
+import React from 'react';
+import { Header } from 'ui/atom/Header';
+import { Footer } from 'ui/atom/Footer';
+// import { FormikComponent, SampleForm } from 'ui/organism/four/FormikComponent';
 export const Fourtemplate = () => {
   return (
     <>
       <Header />
       <div>FourPage</div>
-      <SampleForm />
+      {/* <SampleForm /> */}
       <Footer />
     </>
   );
